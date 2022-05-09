0e46504964e4459623628a527c1b284604fc4b42
Nilanjan <67875846+Gigadown@users.noreply.github.com>
2022-05-09 13:45:25 +0530

    Add files via upload

---
+++ build/precache-manifest.2eb1aec2ecc6586c2524a774d7fbb68f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931e76e2cfbf7691d13cee318fc4b488",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce642e10ee25d52bf9b4",
+    "url": "/static/css/main.a1468374.chunk.css"
+  },
+  {
+    "revision": "45708d2df5f400b51c7e",
+    "url": "/static/js/2.86c46b1e.chunk.js"
+  },
+  {
+    "revision": "5e5c5aaefd726bfc2ad51722d14c5d57",
+    "url": "/static/js/2.86c46b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce642e10ee25d52bf9b4",
+    "url": "/static/js/main.57363dd1.chunk.js"
+  },
+  {
+    "revision": "3eaf75dd1b963f5fe259",
+    "url": "/static/js/runtime-main.5d69f1f3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
